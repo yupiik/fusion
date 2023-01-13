2e947eb83edea9838f79ea6f328bc22b9c96312f
Romain Manni-Bucau <rmannibucau@gmail.com>
2023-01-13 10:43:58 +0100

    Adding some tolerance in tests in terms of discovery to support IDE incremental compilation

---
+++ fusion-testing/src/main/java/io/yupiik/fusion/testing/module/ListingPredicateModule.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2022-2023 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.fusion.testing.module;
+
+import io.yupiik.fusion.framework.api.RuntimeContainer;
+import io.yupiik.fusion.framework.api.container.FusionBean;
+import io.yupiik.fusion.framework.api.container.FusionListener;
+import io.yupiik.fusion.framework.api.container.FusionModule;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class ListingPredicateModule implements FusionModule {
+    private final List<FusionBean<?>> beans = new ArrayList<>();
+    private final List<FusionListener<?>> listeners = new ArrayList<>();
+
+    public ListingPredicateModule(final Predicate<String> beanPredicate,
+                                  final Predicate<String> listenerPredicate,
+                                  final List<Path> dirs) {
+        find(beanPredicate, listenerPredicate, dirs);
+    }
+
+    @Override
+    public int priority() { // "last" - give some space in case of - to ensure we filter others before us to see all beans
+        return Integer.MAX_VALUE - 100;
+    }
+
+    @Override
+    public Stream<FusionBean<?>> beans() {
+        return beans.stream();
+    }
+
+    @Override
+    public Stream<FusionListener<?>> listeners() {
+        try {
+            return listeners.stream();
+        } finally {
+            // last method called in ContainerImpl so just cleanup
+            beans.clear();
+            listeners.clear();
+        }
+    }
+
+    @Override
+    public BiPredicate<RuntimeContainer, FusionBean<?>> beanFilter() {
+        return (container, bean) -> {
+            if (beans.contains(bean)) {
+                return true;
+            }
+            // drop the beans we found but which were registered by another module
+            beans.removeIf(discovered -> Objects.equals(discovered.getClass(), bean.getClass()));
+            return true;
+        };
+    }
+
+    @Override
+    public BiPredicate<RuntimeContainer, FusionListener<?>> listenerFilter() {
+        return (container, listener) -> {
+            if (listeners.contains(listener)) {
+                return true;
+            }
+            // drop the listeners we found but which were registered by another module
+            listeners.removeIf(discovered -> Objects.equals(discovered.getClass(), listener.getClass()));
+            return true;
+        };
+    }
+
+    private void find(final Predicate<String> beanPredicate, final Predicate<String> listenerPredicate, final List<Path> dirs) {
+        final var loader = Thread.currentThread().getContextClassLoader();
+        dirs.forEach(dir -> {
+            try {
+                Files.walkFileTree(dir, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+                        final var filename = file.getFileName().toString();
+                        if (!filename.endsWith(".class")) {
+                            return super.visitFile(file, attrs);
+                        }
+
+                        final var relative = dir.relativize(file).normalize().toString();
+                        if (!relative.startsWith("META-INF")) {
+                            try {
+                                if (beanPredicate.test(filename)) {
+                                    final var clazz = load(loader, toClassName(relative));
+                                    if (clazz != null) {
+                                        beans.add(clazz.asSubclass(FusionBean.class).getConstructor().newInstance());
+                                    }
+                                }
+                                if (listenerPredicate.test(filename)) {
+                                    final var clazz = load(loader, toClassName(relative));
+                                    if (clazz != null) {
+                                        listeners.add(clazz.asSubclass(FusionListener.class).getConstructor().newInstance());
+                                    }
+                                }
+                            } catch (final InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+                                throw new IllegalStateException(e);
+                            } catch (final InvocationTargetException e) {
+                                throw new IllegalStateException(e.getTargetException());
+                            }
+                        }
+
+                        return super.visitFile(file, attrs);
+                    }
+                });
+            } catch (final IOException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private Class<?> load(final ClassLoader loader, final String name) {
+        try {
+            final var clazz = loader.loadClass(name);
+            clazz.getConstructor(); // ensure there is a constructor we can use
+            return clazz;
+        } catch (final ClassNotFoundException | NoSuchMethodException e) {
+            return null;
+        }
+    }
+
+    private String toClassName(final String resource) {
+        return resource.substring(0, resource.length() - ".class".length()).replace(File.separatorChar, '.');
+    }
+}
---
+++ fusion-testing/src/main/java/io/yupiik/fusion/testing/module/TestingModule.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022-2023 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.fusion.testing.module;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class TestingModule extends ListingPredicateModule {
+    public TestingModule() {
+        super(
+                name -> name.endsWith("$FusionBean.class"),
+                name -> name.contains("$FusionListener$"),
+                findDirs());
+    }
+
+    private static List<Path> findDirs() {
+        final var current = Path.of(".");
+        if (Files.exists(current.resolve("pom.xml"))) { // maven, resolve target/classes and target/test-classes
+            return Stream.of(current.resolve("target/classes"), current.resolve("target/test-classes"))
+                    .filter(Files::exists)
+                    .toList();
+        }
+        if (Files.exists(current.resolve("build.gradle"))) { // gradle
+            final var java = current.resolve("build/classes/java");
+            if (Files.exists(java)) {
+                try (final var list = Files.list(java)) {
+                    return list.toList();
+                } catch (final IOException e) {
+                    // no-op, fallback on system prop
+                }
+            }
+        }
+        return Stream.of(System.getProperty(TestingModule.class.getName() + ".dirs", ".").split("\\."))
+                .map(String::strip)
+                .filter(Predicate.not(String::isBlank))
+                .map(Path::of)
+                .filter(Files::exists)
+                .toList();
+    }
+}
