a49f06b0026c92fd974040212fc18d0fadf46963
Romain Manni-Bucau <rmannibucau@gmail.com>
2023-02-25 20:13:30 +0100

    ensure tomcat datasource implements autocloseable to match auto-close logic of the IoC

--- fusion-persistence/src/main/java/io/yupiik/fusion/persistence/impl/datasource/tomcat/TomcatDataSource.java
+++ fusion-persistence/src/main/java/io/yupiik/fusion/persistence/impl/datasource/tomcat/TomcatDataSource.java
@@ -27,7 +27,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.function.Function;
 
-public class TomcatDataSource extends DataSource {
+public class TomcatDataSource extends DataSource implements AutoCloseable {
     private final Class<?>[] connectionProxiedTypes = {Connection.class};
     private final ThreadLocal<Connection> connectionThreadLocal = new ThreadLocal<>();
 
