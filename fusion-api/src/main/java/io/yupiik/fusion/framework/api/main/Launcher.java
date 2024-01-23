e90e783d2df1f7b6d0c3cf034b9781bea5d0668d
Romain Manni-Bucau <rmannibucau@gmail.com>
2024-01-23 21:42:17 +0100

    [launcher] avoid to create anonymous classes for provided beans of launcher main

--- fusion-api/src/main/java/io/yupiik/fusion/framework/api/main/Launcher.java
+++ fusion-api/src/main/java/io/yupiik/fusion/framework/api/main/Launcher.java
@@ -17,12 +17,10 @@ package io.yupiik.fusion.framework.api.main;
 
 import io.yupiik.fusion.framework.api.ConfiguringContainer;
 import io.yupiik.fusion.framework.api.Instance;
-import io.yupiik.fusion.framework.api.RuntimeContainer;
-import io.yupiik.fusion.framework.api.container.bean.BaseBean;
+import io.yupiik.fusion.framework.api.container.bean.ProvidedInstanceBean;
 import io.yupiik.fusion.framework.api.scope.DefaultScoped;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Default launcher using {@link ConfiguringContainer}/{@code io.yupiik.fusion.framework.api.RuntimeContainer}.
@@ -35,18 +33,8 @@ public final class Launcher {
 
     public static void main(final String... args) {
         try (final var container = ConfiguringContainer.of()
-                .register(new BaseBean<Args>(Args.class, DefaultScoped.class, 1000, Map.of()) {
-                    @Override
-                    public Args create(final RuntimeContainer container, final List<Instance<?>> dependents) {
-                        return new Args(List.of(args));
-                    }
-                })
-                .register(new BaseBean<ArgsConfigSource>(ArgsConfigSource.class, DefaultScoped.class, 1000, Map.of()) {
-                    @Override
-                    public ArgsConfigSource create(final RuntimeContainer container, final List<Instance<?>> dependents) {
-                        return new ArgsConfigSource(List.of(args));
-                    }
-                })
+                .register(new ProvidedInstanceBean<>(DefaultScoped.class, Args.class, () -> new Args(List.of(args))))
+                .register(new ProvidedInstanceBean<>(DefaultScoped.class, ArgsConfigSource.class, () -> new ArgsConfigSource(List.of(args))))
                 .start();
              final var awaiters = container.lookups(Awaiter.class, list -> list.stream().map(Instance::instance).toList());) {
             awaiters.instance().forEach(Awaiter::await);
