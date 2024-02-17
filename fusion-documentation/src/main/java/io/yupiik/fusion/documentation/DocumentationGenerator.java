5ca44972eac729789786e7f544ae02e1ffb833d5
Romain Manni-Bucau <rmannibucau@gmail.com>
2024-02-17 20:50:57 +0100

    [doc] enhance documentation generator and document it

--- fusion-documentation/src/main/java/io/yupiik/fusion/documentation/DocumentationGenerator.java
+++ fusion-documentation/src/main/java/io/yupiik/fusion/documentation/DocumentationGenerator.java
@@ -33,6 +33,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static java.util.Collections.enumeration;
+import static java.util.Comparator.comparing;
+import static java.util.Locale.ROOT;
 import static java.util.Objects.requireNonNull;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.joining;
@@ -51,8 +53,6 @@ public class DocumentationGenerator implements Runnable {
     @SuppressWarnings("unchecked")
     public void run() {
         final boolean includeEnv = Boolean.parseBoolean(configuration.get("includeEnvironmentNames"));
-        final var envPattern = includeEnv ? Pattern.compile("[^A-Za-z0-9]") : null;
-
         try (final var json = new JsonMapperImpl(List.of(), c -> Optional.empty())) {
             final var docs = findUrls();
             while (docs.hasMoreElements()) {
@@ -67,37 +67,128 @@ public class DocumentationGenerator implements Runnable {
 
                         final var fileRef = file;
                         final var module = ofNullable(configuration.get("module")).orElseGet(() -> fileRef.substring(fileRef.indexOf('-') + 1, fileRef.indexOf("-1"))); // fusion-$module-$version.jar
-                        final var adoc = sourceBase.resolve("content/_partials/generated/documentation." + module + ".adoc");
+                        final var adoc = ofNullable(configuration.get("output"))
+                                .map(Path::of)
+                                .orElseGet(() -> sourceBase.resolve("content/_partials/generated/documentation." + module + ".adoc"));
+                        final var params = findParameters(classes, roots);
                         Files.createDirectories(adoc.getParent());
-                        Files.writeString(adoc, "= " + module + "\n" +
-                                "\n" +
-                                "== Configuration\n" +
-                                "\n" +
-                                roots.stream()
-                                        .flatMap(rootName -> {
-                                            final var root = (Collection<?>) requireNonNull(classes.get(rootName), () -> "Missing configuration '" + rootName + "'");
-                                            return root.stream()
-                                                    .map(it -> (Map<String, Object>) it)
-                                                    .flatMap(item -> flatten(classes, item))
-                                                    .map(it -> {
-                                                        final var name = it.get("name").toString();
-                                                        return "* `" + name + "`" +
-                                                                (includeEnv ? " (`" + envPattern.matcher(name).replaceAll("_") + "`)" : "") +
-                                                                (Boolean.TRUE.equals(it.get("required")) ? "*" : "") +
-                                                                ofNullable(it.get("defaultValue"))
-                                                                        .map(v -> " (default: `" + v + "`)")
-                                                                        .orElse("") + ": " +
-                                                                it.getOrDefault("documentation", "-") + ".";
-                                                    });
-                                        })
-                                        .sorted()
-                                        .collect(joining("\n")));
+                        Files.writeString(adoc, switch (configuration.getOrDefault("formatter", "default").toLowerCase(ROOT)) {
+                            case "definitionlist" -> definitionList(params, includeEnv);
+                            case "table" -> table(params, includeEnv);
+                            default -> list(params, module, includeEnv);
+                        });
                     }
                 }
             }
         } catch (final IOException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Parameter> findParameters(final Map<?, ?> classes, final List<String> roots) {
+        final var envPattern = Pattern.compile("[^A-Za-z0-9]");
+        return roots.stream()
+                .flatMap(rootName -> {
+                    final var root = (Collection<?>) requireNonNull(classes.get(rootName), () -> "Missing configuration '" + rootName + "'");
+                    return root.stream()
+                            .map(it -> (Map<String, Object>) it)
+                            .flatMap(item -> flatten(classes, item))
+                            .map(it -> {
+                                final var name = it.get("name").toString();
+                                final var documentation = (String) it.get("documentation");
+                                return new Parameter(
+                                        name,
+                                        documentation != null && !documentation.endsWith(".") ? documentation + '.' : documentation,
+                                        it.get("defaultValue"),
+                                        Boolean.TRUE.equals(it.get("required")),
+                                        envPattern.matcher(name).replaceAll("_"));
+                            });
+                })
+                .sorted(comparing(Parameter::name))
+                .toList();
+    }
+
+    protected String table(final List<Parameter> parameters, final boolean includeEnv) {
+        return "[options=\"header\",cols=\"a,a,2a\"]\n" +
+                "|===\n" +
+                "|Name|" + (includeEnv ? "Env Variable|" : "") + "Description|Default\n" +
+                "\n" +
+                parameters.stream()
+                        .map(e -> "| `" + e.name() + "` " + (e.required() ? "*" : "") + '\n' +
+                                (includeEnv ? "| `" + e.envName() + "`\n" : "") +
+                                "| " + e.documentation() + "\n" +
+                                "| " + defaultFor(e.defaultValue()) +
+                                "\n")
+                        .collect(joining("\n")) +
+                "|===\n";
+    }
+
+    protected String list(final List<Parameter> params, final String module, final boolean includeEnv) {
+        return "= " + module + "\n" +
+                "\n" +
+                "== Configuration\n" +
+                "\n" +
+                params.stream()
+                        .map(it -> "* `" + it.name() + "`" +
+                                (includeEnv ? " (`" + it.envName() + "`)" : "") +
+                                (it.required() ? "*" : "") +
+                                ofNullable(it.defaultValue())
+                                        .map(v -> " (default: `" + v + "`)")
+                                        .orElse("") + ": " +
+                                (it.documentation() == null ? "-" : it.documentation()) + ".")
+                        .sorted()
+                        .collect(joining("\n"));
+    }
+
+    protected String definitionList(final List<Parameter> parameters, final boolean includeEnv) {
+        return parameters.stream()
+                .map(e -> e.name() + (includeEnv ? " (env: `" + e.envName() + "`)" : "") + "::\n" +
+                        e.documentation() +
+                        (e.defaultValue() != null ? " Default: " + defaultFor(e.defaultValue()) + "." : "") +
+                        "\n")
+                .collect(joining());
+    }
+
+    protected String defaultFor(final Object defaultValue) {
+        if (defaultValue == null) {
+            return "-";
+        }
+        if (defaultValue instanceof String s) {
+            if (s.startsWith("\"\"\"") && s.endsWith("\"\"\"")) {
+                return formatDefault(s.substring(3, s.length() - 3).stripIndent());
+            }
+            if (s.startsWith("\"") && s.endsWith("\"") && s.indexOf('"', 1) == s.length() - 1) {
+                return formatDefault(s.substring(1, s.length() - 1));
+            }
+        }
+        return formatDefault(defaultValue.toString());
+    }
+
+    protected String formatDefault(final String value) {
+        if (value.contains("\n")) {
+            return "\n[source" + guessLanguage(value) + "]\n----\n" + value + "\n----\n";
+        }
+        return "`" + value + "`";
+    }
+
+    protected String guessLanguage(final String value) {
+        if (value.contains("<html") || value.contains("<div>") || value.contains("<body") || value.contains("<p>")) {
+            return ",html";
+        }
+        if (value.contains("\nconst ") || value.contains("\nlet ") || value.startsWith("const ") || value.startsWith("let ")) {
+            return ",javascript";
+        }
+        if (value.contains("\nclass ") || value.startsWith("class ")) {
+            return ",java";
+        }
+        if (value.contains("</") || value.contains("/>")) {
+            return ",xml";
+        }
+        if (value.contains(":\n")) {
+            return ",yaml";
+        }
+        return "";
     }
 
     private Enumeration<URL> findUrls() throws IOException {
@@ -110,6 +201,16 @@ public class DocumentationGenerator implements Runnable {
                         try {
                             return new URL(it);
                         } catch (final MalformedURLException e) {
+                            try {
+                                final var path = Path.of(it);
+                                if (Files.exists(path)) {
+                                    return path.toUri().toURL();
+                                }
+                            } catch (final RuntimeException re) {
+                                // no-op
+                            } catch (final MalformedURLException ex) {
+                                throw new IllegalStateException(ex);
+                            }
                             throw new IllegalStateException(e);
                         }
                     })
@@ -133,5 +234,10 @@ public class DocumentationGenerator implements Runnable {
                                 it.entrySet().stream().filter(i -> !"name".equals(i.getKey())))
                         .collect(toMap(Map.Entry::getKey, i -> i.getValue() == null ? Map.of() : i.getValue())))
                 .flatMap(it -> flatten(classes, it));
+    }
+
+    protected record Parameter(String name, String documentation,
+                               Object defaultValue, boolean required,
+                               String envName) {
     }
 }
---
+++ fusion-documentation/src/test/java/io/yupiik/fusion/documentation/DocumentationGeneratorTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2022 - present - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.fusion.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DocumentationGeneratorTest {
+    @Test
+    void defaultFormatting(@TempDir final Path work) throws IOException {
+        final var url = writeConf(work);
+        final var output = work.resolve("output.adoc");
+        new DocumentationGenerator(Files.createDirectories(work.resolve("base")), Map.of(
+                "module", "test-module",
+                "urls", url.toExternalForm(),
+                "includeEnvironmentNames", "true",
+                "output", output.toString()))
+                .run();
+        assertEquals("""
+                = test-module
+                                
+                == Configuration
+                                
+                * `jwt.algo` (`jwt_algo`) (default: `"RS256"`): JWT `alg` value..
+                * `jwt.expRequired` (`jwt_expRequired`) (default: `true`): Are `exp` (expiry) validation required of can it be skipped if claim is missing..""", Files.readString(output));
+    }
+
+    @Test
+    void tableFormatting(@TempDir final Path work) throws IOException {
+        final var url = writeConf(work);
+        final var output = work.resolve("output.adoc");
+        new DocumentationGenerator(Files.createDirectories(work.resolve("base")), Map.of(
+                "formatter", "table",
+                "module", "test-module",
+                "urls", url.toExternalForm(),
+                "includeEnvironmentNames", "true",
+                "output", output.toString()))
+                .run();
+        assertEquals("""
+                [options="header",cols="a,a,2a"]
+                |===
+                |Name|Env Variable|Description|Default
+                                
+                | `jwt.algo`\s
+                | `jwt_algo`
+                | JWT `alg` value.
+                | `RS256`
+                
+                | `jwt.expRequired`\s
+                | `jwt_expRequired`
+                | Are `exp` (expiry) validation required of can it be skipped if claim is missing.
+                | `true`
+                |===
+                """, Files.readString(output));
+    }
+
+    @Test
+    void definitionListFormatting(@TempDir final Path work) throws IOException {
+        final var url = writeConf(work);
+        final var output = work.resolve("output.adoc");
+        new DocumentationGenerator(Files.createDirectories(work.resolve("base")), Map.of(
+                "formatter", "definitionList",
+                "module", "test-module",
+                "urls", url.toExternalForm(),
+                "includeEnvironmentNames", "true",
+                "output", output.toString()))
+                .run();
+        assertEquals("""
+                jwt.algo (env: `jwt_algo`)::
+                JWT `alg` value. Default: `RS256`.
+                jwt.expRequired (env: `jwt_expRequired`)::
+                Are `exp` (expiry) validation required of can it be skipped if claim is missing. Default: `true`.
+                """, Files.readString(output));
+    }
+
+    private URL writeConf(final Path work) throws IOException {
+        return Files.writeString(work.resolve("doc.json"), """
+                        {
+                           "version":1,
+                           "classes":{
+                              "io.yupiik.test.JwtValidatorConfiguration":[
+                                 {
+                                    "name":"jwt.algo",
+                                    "documentation":"JWT `alg` value.",
+                                    "defaultValue":"\\"RS256\\"",
+                                    "required":false
+                                 },
+                                 {
+                                    "name":"jwt.expRequired",
+                                    "documentation":"Are `exp` (expiry) validation required of can it be skipped if claim is missing.",
+                                    "defaultValue":true,
+                                    "required":false
+                                 }
+                              ]
+                           },
+                           "roots":[
+                              "io.yupiik.test.JwtValidatorConfiguration"
+                           ]
+                        }""")
+                .toUri().toURL();
+    }
+}
