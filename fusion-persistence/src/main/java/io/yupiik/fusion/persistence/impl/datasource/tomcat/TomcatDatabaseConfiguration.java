97a25418ed21cb89a3c204710440a7e06365e5a9
Romain Manni-Bucau <rmannibucau@gmail.com>
2024-02-19 14:58:15 +0100

    [persistence] ensure TomcatDatabaseConfiguration is embeddable in a root configuration friendly with documentation

--- fusion-persistence/src/main/java/io/yupiik/fusion/persistence/impl/datasource/tomcat/TomcatDatabaseConfiguration.java
+++ fusion-persistence/src/main/java/io/yupiik/fusion/persistence/impl/datasource/tomcat/TomcatDatabaseConfiguration.java
@@ -15,24 +15,38 @@
  */
 package io.yupiik.fusion.persistence.impl.datasource.tomcat;
 
+import io.yupiik.fusion.framework.build.api.configuration.Property;
+
+// generic configuration which can be reused in a root configuration to create a database
 public record TomcatDatabaseConfiguration(
-        String driver,
-        String url,
-        String username,
-        String password,
+        @Property(documentation = "JDBC driver to use to get connections.") String driver,
+        @Property(documentation = "JDBC URL.") String url,
+        @Property(documentation = "Database username.") String username,
+        @Property(documentation = "Database password.") String password,
+        @Property(documentation = "Should connections be tested when retrieved from the pool.", defaultValue = "false")
         boolean testOnBorrow,
+        @Property(documentation = "Should connections be tested when returning to the pool.", defaultValue = "false")
         boolean testOnReturn,
+        @Property(documentation = "Should connections be tested when not used and in the pool.", defaultValue = "true")
         boolean testWhileIdle,
+        @Property(documentation = "Timeout (ms) between connection tests.", defaultValue = "5000")
         int timeBetweenEvictionRuns,
+        @Property(documentation = "Timeout before any eviction test for a connection.", defaultValue = "60000")
         int minEvictableIdleTime,
+        @Property(documentation = "Test query for eviction - if not set the driver one is used.")
         String validationQuery,
+        @Property(documentation = "Default timeout for validations.", defaultValue = "30")
         int validationQueryTimeout,
+        @Property(documentation = "Min number of connection - even when nothing happens.", defaultValue = "2")
         int minIdle,
+        @Property(documentation = "Max active connections.", defaultValue = "100")
         int maxActive,
+        @Property(documentation = "Should detected as abandonned connections be dropped.", defaultValue = "false")
         boolean removeAbandoned,
-        Boolean defaultAutoCommit,
-        Boolean logAbandoned,
-        int removeAbandonedTimeout,
+        @Property(documentation = "Should autocommit be used.") Boolean defaultAutoCommit,
+        @Property(documentation = "Should abandons be logged.") Boolean logAbandoned,
+        @Property(documentation = "Abandon timeout.", defaultValue = "60") int removeAbandonedTimeout,
+        @Property(documentation = "Should a rollback be applied when returning to the pool.", defaultValue = "false")
         boolean rollbackOnReturn) {
     public TomcatDatabaseConfiguration(final String driver, final String url, final String username, final String password,
                                        final boolean testOnBorrow, final boolean testOnReturn, final boolean testWhileIdle,
