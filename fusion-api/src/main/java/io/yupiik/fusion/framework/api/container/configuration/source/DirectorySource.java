9d8ba4cd84017a07969fe203ab9ab50429f619a1
Romain Manni-Bucau <rmannibucau@gmail.com>
2025-04-13 14:26:37 +0200

    Enable to automatically support file based configuration (kubernetes secrets)

---
+++ fusion-api/src/main/java/io/yupiik/fusion/framework/api/container/configuration/source/DirectorySource.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 - present - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.fusion.framework.api.container.configuration.source;
+
+import io.yupiik.fusion.framework.api.configuration.ConfigurationSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Map.entry;
+import static java.util.stream.Collectors.toMap;
+
+// can be used for k8s secrets for example
+// note it is immutable for now since config reloading triggers consistency issues in apps.
+// if needed it can use a custom source (facading this one with cache for ex)
+public class DirectorySource implements ConfigurationSource {
+    private final String prefix;
+    private final Map<String, String> values = new HashMap<>();
+
+    public DirectorySource(final String prefix, final Path directory) {
+        this.prefix = prefix;
+        if (!Files.exists(directory)) {
+            return;
+        }
+        try (final var child = Files.list(directory)) {
+            values.putAll(child
+                    .filter(Files::isRegularFile)
+                    .map(it -> {
+                        try {
+                            return entry(it.getFileName().toString(), Files.readString(it, UTF_8));
+                        } catch (final IOException ioe) {
+                            Logger.getLogger(getClass().getName()).log(Level.FINER, ioe, ioe::getMessage);
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(toMap(Map.Entry::getKey, Map.Entry::getValue)));
+        } catch (final IOException e) {
+            Logger.getLogger(getClass().getName()).log(Level.FINER, e, e::getMessage);
+        }
+    }
+
+    @Override
+    public String get(final String key) {
+        if (key.startsWith(prefix)) {
+            return values.get(key.substring(prefix.length()));
+        }
+        return null;
+    }
+}
