2498610db4662d35ce661679556a69d8490574d2
Romain Manni-Bucau <rmannibucau@gmail.com>
2026-01-18 11:56:26 +0100

    [handlebars] support dotted notation

--- fusion-handlebars/src/main/java/io/yupiik/fusion/framework/handlebars/compiler/accessor/MapAccessor.java
+++ fusion-handlebars/src/main/java/io/yupiik/fusion/framework/handlebars/compiler/accessor/MapAccessor.java
@@ -17,18 +17,52 @@ package io.yupiik.fusion.framework.handlebars.compiler.accessor;
 
 import io.yupiik.fusion.framework.handlebars.spi.Accessor;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class MapAccessor implements Accessor {
     public Object find(final Object data, final String name) {
+        if (data instanceof List<?> list) {
+            return handleList(list, name);
+        }
+
+        return handleMap(data, name);
+    }
+
+    private Object handleList(final List<?> list, final String name) {
+        final var sep = name.indexOf('.');
+        if (isDottedAccessor(name, sep)) {
+            return find(find(list, name.substring(0, sep)), name.substring(sep + 1));
+        }
+
+        try {
+            final var idx = Integer.parseInt(name);
+            return list.size() > idx ? list.get(idx) : null;
+        } catch (final NumberFormatException nfe) {
+            return null;
+        }
+    }
+
+    private Object handleMap(Object data, String name) {
         if (!(data instanceof Map<?, ?> map)) {
             throw new IllegalArgumentException("Unsupported type '" + data + "'");
         }
-        final Object value = map.get(name);
+        var value = map.get(name); // optimistic case, avoid to browse the string is useless
+        if (value == null) {
+            final var sep = name.indexOf('.');
+            if (isDottedAccessor(name, sep)) {
+                return find(find(data, name.substring(0, sep)), name.substring(sep + 1));
+            }
+        }
+
         if (value instanceof Supplier<?> s) {
             return s.get();
         }
         return value;
+    }
+
+    private boolean isDottedAccessor(final String name, final int nextDot) {
+        return nextDot > 0 && nextDot < name.length() - 1;
     }
 }
